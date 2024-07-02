7fbb3be81f082aa704cec9897fedab4b744e7c94
eddy <80455207+1eddy@users.noreply.github.com>
2024-07-02 11:47:46 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,41 @@
 
+const { CosmosClient } = require("@azure/cosmos");
+
+const endpoint = process.env.COSMOS_DB_ENDPOINT;
+const key = process.env.COSMOS_DB_KEY;
+const databaseId = "DebtCollectionDB";
+const containerId = "DebtItems";
+const client = new CosmosClient({ endpoint, key });
+
+module.exports = async function (context, req) {
+  context.log('JavaScript HTTP trigger function processed a request.');
+
+  if (req.method === "GET") {
+    const { resources: items } = await client
+      .database(databaseId)
+      .container(containerId)
+      .items.query("SELECT * from c")
+      .fetchAll();
+
+    context.res = {
+      status: 200,
+      body: items
+    };
+  } else if (req.method === "POST") {
+    const { id, description, amount } = req.body;
+    const { resource: createdItem } = await client
+      .database(databaseId)
+      .container(containerId)
+      .items.create({ id, description, amount });
+
+    context.res = {
+      status: 201,
+      body: createdItem
+    };
+  } else {
+    context.res = {
+      status: 405,
+      body: "Method Not Allowed"
+    };
+  }
+};
